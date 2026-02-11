e40b5e79d309b71ea3430a0bbfd904111dfcf67a
yijunge-ucb <yijunge@berkeley.edu>
2026-02-11 11:56:48 -0800

    Change pip cache location to temporary directory
    Use a temporary directory for XDG_CACHE_HOME instead of the default pip cache location.

--- install-miniforge.bash
+++ install-miniforge.bash
@@ -8,6 +8,8 @@ MINIFORGE_VERSION='24.11.2-1'
 URL="https://github.com/conda-forge/miniforge/releases/download/${MINIFORGE_VERSION}/Miniforge3-${MINIFORGE_VERSION}-Linux-x86_64.sh"
 INSTALLER_PATH=/tmp/miniforge-installer.sh
 
+export XDG_CACHE_HOME=$(mktemp)
+
 wget --quiet $URL -O ${INSTALLER_PATH}
 chmod +x ${INSTALLER_PATH}
 
@@ -31,8 +33,6 @@ conda clean --all -f -y
 rm ${INSTALLER_PATH}
 
 # Remove the pip cache created as part of installing mambaforge
-rm -rf /root/.cache
-
-chown -Rh $NB_USER:$NB_USER ${CONDA_DIR}
+rm -rf ${XDG_CACHE_HOME}
 
 conda list -n root
